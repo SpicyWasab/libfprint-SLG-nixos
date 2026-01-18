37bdb0c058b3f189adb4337e21b4027d123f65ff
Wasab <spicywasab+github@proton.me>
2026-01-18 18:32:50 +0100

    fix: forgot one thing, name is now libfprint

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/libfprint { };
+  libfprint = pkgs.callPackage ./pkgs/libfprint { };
   # libfprint-tod = pkgs.callPackage ./pkgs/libfprint-tod { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
